a668c5c61495d1d0843f0bfaefa2fbc5623700e1
Yury Krikun <yury.krikun@rcslabs.ru>
2018-05-18 20:52:20 +0300

    ui

---
+++ ui/src/components/PadButton.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ToggleButton from './ToggleButton';
+
+export default class PadButton extends ToggleButton {
+    constructor(props) {
+        super(props);
+        // this.state = {};
+    }
+}
---
+++ ui/src/components/PianoRoll.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PianoRoll extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyWidth : 16
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default PianoRoll;
