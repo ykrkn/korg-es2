e26aa0ee3e3108f09d11cd4ca21b1483ab654470
Yury Krikun <yury.krikun@rcslabs.ru>
2018-08-28 18:28:58 +0300

    ui sx scratched

--- ui/src/service.js
+++ ui/src/service.js
@@ -16,20 +16,117 @@ var data_mock_1 = require("./data-mock");
 // get all dump
 // get file dump
 var SXObject = /** @class */ (function () {
-    function SXObject() {
+    function SXObject(data) {
+        this.data = data;
     }
-    SXObject.prototype.unpack = function (data) { };
-    SXObject.prototype.pack = function () { return []; };
+    SXObject.prototype.pack = function () { return new Uint8Array(0); };
     return SXObject;
+}());
+var DataChunk = /** @class */ (function () {
+    function DataChunk() {
+    }
+    DataChunk.byteArray = function (name, from, length) {
+        var p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = length;
+        p.ba2val = function (ba) { return ba.slice(from, from + length); };
+        p.val2ba = function (v) { return v.slice(0); };
+        return p;
+    };
+    DataChunk.byte = function (name, from) {
+        var p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = 1;
+        p.ba2val = function (ba) { return ba[from]; };
+        p.val2ba = function (v) { return [v]; };
+        return p;
+    };
+    DataChunk.short = function (name, from) {
+        var p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = 2;
+        p.ba2val = function (ba) {
+            var _ba = ba.slice(from, from + 2);
+            return (((_ba[1] & 0xFF) << 8) | _ba[0] & 0xFF);
+        };
+        p.val2ba = null; // TODO:
+        return p;
+    };
+    DataChunk.int = function (name, from) {
+        var p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = 4;
+        p.ba2val = function (ba) {
+            var _ba = ba.slice(from, from + 4);
+            return ((_ba[3] & 0xFF) << 24) | ((_ba[2] & 0xFF) << 16) | ((_ba[1] & 0xFF) << 8) | _ba[0] & 0xFF;
+        };
+        p.val2ba = null; // TODO:
+        return p;
+    };
+    DataChunk.string = function (name, from, length) {
+        var p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = length;
+        p.ba2val = function (ba) {
+            var s = "";
+            for (var i = 0; i < length; i++) {
+                var n = ba[from + i];
+                if (n == 0)
+                    break;
+                s = s.concat(String.fromCharCode(n));
+            }
+            return s;
+        };
+        p.val2ba = null; // TODO:
+        return p;
+    };
+    return DataChunk;
 }());
 var Pattern = /** @class */ (function (_super) {
     __extends(Pattern, _super);
-    function Pattern() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function Pattern(data) {
+        var _this = _super.call(this, data) || this;
+        _this.props = {};
+        for (var i in Pattern.chunks) {
+            var chunk = Pattern.chunks[i];
+            _this.props[chunk.name] = chunk.ba2val(data);
+        }
+        return _this;
     }
-    Pattern.prototype.unpack = function (data) {
-        this.data = data;
-    };
+    Pattern.chunks = [
+        DataChunk.string("header", 0, 4),
+        DataChunk.int("size", 4),
+        DataChunk.byteArray("version", 12, 2),
+        DataChunk.string("patternName", 16, 18),
+        DataChunk.short("tempo10x", 34),
+        DataChunk.byte("swing", 36),
+        DataChunk.byte("length", 37),
+        DataChunk.byte("beat", 38),
+        DataChunk.byte("key", 39),
+        DataChunk.byte("scale", 40),
+        DataChunk.byte("chordset", 41),
+        DataChunk.byte("playLevel", 42),
+        // touch scale offset = 44
+        DataChunk.byte("gateArpPattern", 44 + 5),
+        DataChunk.byte("gateArpSpeed", 44 + 6),
+        // TODO: check gateArpTime! should be -100 ~ 100
+        DataChunk.short("gateArpTime", 44 + 8),
+        // Master Fx offset = 60
+        DataChunk.byte("masterFxType", 61),
+        DataChunk.byte("masterFxPadX", 62),
+        DataChunk.byte("masterFxPadY", 63),
+        DataChunk.byte("masterFxHold", 65),
+        DataChunk.byte("alt1314", 68),
+        DataChunk.byte("alt1516", 69),
+        // TODO: motion seq
+        // TODO: parts
+        DataChunk.string("footer", 15356, 4)
+    ];
     return Pattern;
 }(SXObject));
 var Dataset = /** @class */ (function (_super) {
@@ -45,10 +142,8 @@ var ES2Service = /** @class */ (function () {
     function ES2Service() {
     }
     ES2Service.prototype.getPattern = function () {
-        var pattern = new Pattern();
         var data = data_mock_1.default();
-        pattern.unpack(data);
-        return pattern;
+        return new Pattern(data);
     };
     return ES2Service;
 }());
--- ui/src/service.ts
+++ ui/src/service.ts
@@ -6,14 +6,126 @@ import getData from './data-mock';
 // get file dump
 
 class SXObject {
-    protected data:number[];
-    unpack(data:number[]) {}
-    pack():number[] { return []; }
+    protected data:Uint8Array;
+    constructor(data:Uint8Array) {
+        this.data = data;
+    }
+    pack():Uint8Array { return new Uint8Array(0); }
+}
+
+class DataChunk {
+
+    public offset:number;
+    public length:number;
+    public name:string;
+    public ba2val:Function;
+    public val2ba:Function;
+
+    static byteArray(name, from, length):DataChunk {
+        const p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = length;
+        p.ba2val = (ba) => ba.slice(from, from+length);
+        p.val2ba = (v) => v.slice(0);
+        return p;
+    }
+
+    static byte(name, from):DataChunk {
+        const p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = 1;
+        p.ba2val = (ba) => ba[from];
+        p.val2ba = (v) => [v];
+        return p;
+    }
+
+    static short(name, from):DataChunk {
+        const p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = 2;
+        p.ba2val = (ba) => {
+            const _ba = ba.slice(from, from+2);
+            return (((_ba[1] & 0xFF) << 8) | _ba[0] & 0xFF);
+        }
+        p.val2ba = null; // TODO:
+        return p;
+    }
+
+    static int(name, from):DataChunk {
+        const p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = 4;
+        p.ba2val = (ba) => {
+            const _ba = ba.slice(from, from+4);
+            return ((_ba[3] & 0xFF) << 24) | ((_ba[2] & 0xFF) << 16) | ((_ba[1] & 0xFF) << 8) | _ba[0] & 0xFF;
+        }
+        p.val2ba = null; // TODO:
+        return p;
+    }
+
+    static string(name, from, length):DataChunk {
+        const p = new DataChunk();
+        p.name = name;
+        p.offset = from;
+        p.length = length;
+        p.ba2val = (ba) => {
+            let s = "";
+            for (let i = 0; i < length; i++) {
+                let n = ba[from+i];
+                if(n == 0) break;
+                s = s.concat(String.fromCharCode(n));
+            }
+            return s;
+        };
+        p.val2ba = null; // TODO:
+        return p;
+    }
 }
 
 class Pattern extends SXObject {
-    unpack(data:number[]) {
-        this.data = data;
+    static chunks = [
+        DataChunk.string("header", 0, 4),
+        DataChunk.int("size", 4),
+        DataChunk.byteArray("version", 12, 2),
+        DataChunk.string("patternName", 16, 18),
+        DataChunk.short("tempo10x", 34),
+        DataChunk.byte("swing", 36),
+        DataChunk.byte("length", 37),
+        DataChunk.byte("beat", 38),
+        DataChunk.byte("key", 39),
+        DataChunk.byte("scale", 40),
+        DataChunk.byte("chordset", 41),
+        DataChunk.byte("playLevel", 42),
+        // touch scale offset = 44
+        DataChunk.byte("gateArpPattern", 44+5),
+        DataChunk.byte("gateArpSpeed", 44+6),
+        // TODO: check gateArpTime! should be -100 ~ 100
+        DataChunk.short("gateArpTime", 44+8),
+        // Master Fx offset = 60
+        DataChunk.byte("masterFxType", 61),
+        DataChunk.byte("masterFxPadX", 62),
+        DataChunk.byte("masterFxPadY", 63),
+        DataChunk.byte("masterFxHold", 65),
+        DataChunk.byte("alt1314", 68),
+        DataChunk.byte("alt1516", 69),
+        // TODO: motion seq
+        // TODO: parts
+        DataChunk.string("footer", 15356, 4)
+    ];
+
+    private props;
+
+    constructor(data:Uint8Array) {
+        super(data);
+        this.props = {};
+        for(let i in Pattern.chunks) {
+            let chunk = Pattern.chunks[i];
+            this.props[chunk.name] = chunk.ba2val(data);
+        }
     }
 }
 
@@ -23,9 +135,7 @@ class Dataset extends SXObject {
 
 export class ES2Service {
     public getPattern():Pattern {
-        const pattern = new Pattern();    
         const data = getData();
-        pattern.unpack(data);
-        return pattern;
+        return new Pattern(data); 
     }
 }
