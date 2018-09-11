19494985d8e8a6d6493f669d0471a266793939d7
Yury Krikun <yury.krikun@rcslabs.ru>
2018-09-11 21:20:00 +0300

    ui

--- ui/src/components/PadButton.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ToggleButton from './ToggleButton';
-
-export default class PadButton extends ToggleButton {
-    constructor(props) {
-        super(props);
-        // this.state = {};
-    }
-}
