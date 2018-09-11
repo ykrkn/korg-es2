957022dd8299dc58cce50d23e493959ff0f9558c
Yury Krikun <yury.krikun@rcslabs.ru>
2018-09-11 20:05:05 +0300

    dump to json convert

--- es2ed/main.c
+++ es2ed/main.c
@@ -1,4 +1,4 @@
-//  gcc main.c && ./a.out
+//  gcc main.c && ./a.out ../assets/data.bin /tmp/out.json
 //
 //  main.c
 //  es2ed
@@ -21,46 +21,58 @@ int uint16le (const byte * p) {
     return (hi << 8) | lo;
 }
 
-void print_step(korg_e2_step step) {
-    printf("{\"step_on\":%d, \"gate_time\":%d, \"velocity\":%d, \"trigger_on\":%d, ", 
+void print_step(FILE *fout, korg_e2_step step) {
+    fprintf(fout, "{\"step_on\":%d, \"gate_time\":%d, \"velocity\":%d, \"trigger_on\":%d, ", 
         step.step_on_off, step.step_gate_time, step.step_velocity, step.step_trigger_on_off);  
-    printf("\"notes\":[%d,%d,%d,%d]", step.step_note_slot1-1, step.step_note_slot2-1, step.step_note_slot3-1, step.step_note_slot4-1);   
-    printf("}"); 
+    fprintf(fout, "\"notes\":[%d,%d,%d,%d]", step.step_note_slot1-1, step.step_note_slot2-1, step.step_note_slot3-1, step.step_note_slot4-1);   
+    fprintf(fout, "}"); 
 }
 
-void print_part(korg_e2_part part) {
-    printf("{\"last_step\":%d, \"oscillator_type\":%d, \"steps\":[", (part.last_step == 0 ? 16 : part.last_step), uint16le(part.oscillator_type));
+void print_part(FILE *fout, korg_e2_part part) {
+    fprintf(fout, "{\"last_step\":%d, \"oscillator_type\":%d, \"steps\":[", (part.last_step == 0 ? 16 : part.last_step), uint16le(part.oscillator_type));
     for(int k=0; k<64; ++k) { // steps 
-        if (k > 0) printf(",");
-        print_step(part.steps[k]);  
+        if (k > 0) fprintf(fout, ",");
+        print_step(fout, part.steps[k]);  
     }
-    printf("]");
-    printf("}"); 
+    fprintf(fout, "]");
+    fprintf(fout, "}"); 
 }
 
-void print_pattern(korg_e2_pattern ptn) {
-    printf("{\"name\":\"%s\", \"tempo\":%.1f, ", ptn.name, uint16le(ptn.tempo)*0.1);
-    printf("\"length\":%d, \"beat\":%d, \"key\":%d, \"scale\":%d, \"chordset\":%d, \"level\":%d, \"parts\":[", 
+void print_pattern(FILE *fout, korg_e2_pattern ptn) {
+    fprintf(fout, "{\"name\":\"%s\", \"tempo\":%.1f, ", ptn.name, uint16le(ptn.tempo)*0.1);
+    fprintf(fout, "\"length\":%d, \"beat\":%d, \"key\":%d, \"scale\":%d, \"chordset\":%d, \"level\":%d, \"parts\":[", 
           ptn.length+1, ptn.beat, ptn.key, ptn.scale, ptn.chordset,127-ptn.level);
 
-    byte *a = &(ptn);
     for(int j=0; j<16; ++j) { // parts
-        if (j > 0) printf(",");
-        print_part(ptn.parts[j]);
+        if (j > 0) fprintf(fout, ",");
+        //print_part(fout, ptn.parts[j]);
     }
-    printf("]");
-    printf("}");
+    fprintf(fout, "]");
+    fprintf(fout, "}");
 }
 
 int main(int argc, const char * argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s file_in file_out\n", argv[0]);
+        return 0;
+    }
+
+    printf("Read dump from %s into %s\n", argv[1], argv[2]);
+
     korg_e2_pattern p[PATTERNS_COUNT];
-    //printf("%lu\n", PATTERN_SIZE);
+    //fprintf(fout, "%lu\n", PATTERN_SIZE);
     
-    FILE *fptr;
-    fptr = fopen("/Users/yurykrikun/src/korg-es2/assets/data.bin", "r");
-    
+    FILE *fptr = fopen(argv[1], "r");
+
     if (fptr == NULL) {
-        printf("File could not be opened, exiting program.\n");
+        printf("File %s could not be opened, exiting program.\n", argv[1]);
+        return 0;
+    }
+
+    FILE *fout = fopen(argv[2], "w");
+
+    if (fout == NULL) {
+        printf("File %s could not be opened, exiting program.\n", argv[2]);
         return 0;
     }
 
@@ -70,11 +82,12 @@ int main(int argc, const char * argv[]) {
     }
     fclose(fptr);
 
-    printf("[");
+    fprintf(fout, "[");
     for(int i=0; i<PATTERNS_COUNT; ++i) {
-        if (i > 0) printf(",");
-        print_pattern(p[i]);
+        if (i > 0) fprintf(fout, ",");
+        print_pattern(fout, p[i]);
     }
-    printf("]\n");
+    fprintf(fout, "]\n");
+    fclose(fout);
     return 0;
 }
