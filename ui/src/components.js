a1b47a6d04d32cf7694c0fc98f34ca360a881a09
Yury Krikun <yury.krikun@gmail.com>
2019-03-31 00:20:44 +0300

    reset selected on init

--- ui/src/components.js
+++ ui/src/components.js
@@ -212,6 +212,36 @@ class PartDetails extends Component {
     return <div className={'part-details ' + (visible ? 'visible' : 'hidden')}>
       <button class='open' onClick={() => this.setState({visible : true})}>&rarr;</button>
       <button class='close' onClick={() => this.setState({visible : false})}>&larr;</button>
+
+    ['last_step',1], // 0,1~15=16,1~15
+    ['mute',1], // 0,1=OFF,ON
+    ['voice_assign',1], // 0,1,2,3=Mono1, Mono2, Poly1, Poly2
+    ['motion_sequence',1], // 0,1,2=Off, Smooth, TriggerHold
+    ['trig_pad_velocity',1], // 0,1=Off,On
+    ['scale_mode',1], // 0,1=Off,On
+    ['part_priority',1], // 0,1=Normal,High
+    ['oscillator_type',2], // 0~500
+    ['oscillator_edit',1], // 0~127
+    ['filter_type',1], // 0~16
+    ['filter_cutoff',1], // 0~127
+    ['filter_resonance',1], // 0~127
+    ['filter_eg_int',1], // -63~63
+    ['modulation_type',1], // 0~71
+    ['modulation_speed',1], // 0~127
+    ['modulation_depth',1], // 0~127
+    ['eg_attack',1], // 0~127
+    ['eg_decay_release',1], // 0~127
+    ['amp_level',1], // 0~127
+    ['amp_pan',1], // -63~0~64=L63~center~R63
+    ['eg_on_off',1], // 0,1=Off,On
+    ['mfx_send_on_off',1], // 0,1=Off,On
+    ['groove_type',1], // 0~24
+    ['groove_depth',1], // 0~127
+    ['ifx_on_off',1], // 0,1=Off,On
+    ['ifx_type',1], // 0~37
+    ['ifx_edit',1], // 0~127
+    ['oscillator_pitch',1], // -63~+63
+    ['oscillator_glide',1], // 0~127
     </div>;
   }
 }
@@ -220,10 +250,24 @@ class Pattern extends Component {
     constructor(props) {
         super(props);
         this.state = {
+          data : null,
           selectedBar : 0,
           selectedPart : -1,
           patternLength : 1,
         }
+    }
+
+    static getDerivedStateFromProps(props, state) {      
+      if (state.data != props.data) {
+        const { data } = props; 
+        console.log('getDerivedStateFromProps');
+        return {
+          data : data,
+          selectedBar : 0,
+          selectedPart : -1,
+        };
+      }
+      return null;
     }
 
     renderBarButton(idx) {
